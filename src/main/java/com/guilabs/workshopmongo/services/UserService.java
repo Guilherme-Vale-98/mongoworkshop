707d904c2fb5ad312d639d5332857d8f6342cf58
Guilherme Vale <guilherme.vale98@gmail.com>
2023-06-13 17:03:22 -0300

    Created findUserById method

--- src/main/java/com/guilabs/workshopmongo/services/UserService.java
+++ src/main/java/com/guilabs/workshopmongo/services/UserService.java
@@ -1,11 +1,13 @@
 package com.guilabs.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilabs.workshopmongo.domain.User;
+import com.guilabs.workshopmongo.exceptions.NotFoundException;
 import com.guilabs.workshopmongo.repositories.UserRepository;
 
 @Service
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new NotFoundException("Objeto não encontrado"));
 	}
 	
 }
